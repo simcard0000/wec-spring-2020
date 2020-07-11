01889695a6d06c17ce7e548afbb5487ced010f7a
a-gratton <gratton.aidan@gmail.com>
2020-07-11 08:44:32 -0600

    Implemented getSuggestions Interface

--- autocomplete.py
+++ autocomplete.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 key = ''
 url = "https://eastus.api.cognitive.microsoft.com/bing/v7.0/suggestions"
@@ -7,16 +8,16 @@ with open('auth.txt', 'r') as file:
     data = file.read().split(' ')
     for i, x in enumerate(data):
         if x == 'Ocp-Apim-Subscription-Key':
-            key = data[i+1]
+            key = data[i + 1]
 
-
-payload = {}
-params = {'q': "hello"}
 headers = {
-'Ocp-Apim-Subscription-Key': key
+    'Ocp-Apim-Subscription-Key': key
 }
 
 
-response = requests.get(url, headers=headers, params=params, data=payload)
-
-print(response.content)
+def getSuggestions(query: str) -> list:
+    params = {'q': query}
+    response = requests.get(url, headers=headers, params=params)
+    response_dict = json.loads(response.content)
+    suggestion_list = [x['displayText'] for x in response_dict['suggestionGroups'][0]['searchSuggestions']]
+    return suggestion_list
