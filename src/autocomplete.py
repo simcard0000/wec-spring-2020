e3efbc14b733f94069818f4e2ccf95844109b6d7
a-gratton <gratton.aidan@gmail.com>
2020-07-11 10:57:17 -0600

    Moved to src Directory

--- src/autocomplete.py
+++ src/autocomplete.py
@@ -4,7 +4,7 @@ import json
 key = ''
 url = "https://eastus.api.cognitive.microsoft.com/bing/v7.0/suggestions"
 
-with open('auth.txt', 'r') as file:
+with open('../auth.txt', 'r') as file:
     data = file.read().split(' ')
     for i, x in enumerate(data):
         if x == 'Ocp-Apim-Subscription-Key':
