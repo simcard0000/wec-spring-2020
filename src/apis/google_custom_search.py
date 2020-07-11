2f1fcc3e4ba63715b7c2b92bada914d59e388fb0
a-gratton <gratton.aidan@gmail.com>
2020-07-11 12:04:35 -0600

    Wrap APIs in Class for Module Importing

--- src/apis/google_custom_search.py
+++ src/apis/google_custom_search.py
@@ -3,68 +3,69 @@ import os
 import json
 from dotenv import load_dotenv, find_dotenv
 
-URL = 'https://www.googleapis.com/customsearch/v1?'
 
-# Google custom search engine credentials
-def get_credentials() -> tuple:
-    load_dotenv(find_dotenv())
-    return os.getenv('GOOGLE_API_KEY'), os.getenv('GOOGLE_CUSTOM_SEARCH_ENGINE_ID')
+class GoogleCustomSearch():
+    def __init__(self):
+        self.URL = 'https://www.googleapis.com/customsearch/v1?'
 
+    # Google custom search engine credentials
+    def get_credentials(self) -> tuple:
+        load_dotenv(find_dotenv())
+        return os.getenv('GOOGLE_API_KEY'), os.getenv('GOOGLE_CUSTOM_SEARCH_ENGINE_ID')
 
-# Extracts only relevant content from results json and returns a 
-# list of dict objects each containing the following content:
-# - title
-# - snippet
-# - url (aka formatted url)
-def get_filtered_results(results_json: dict) -> list:
-    filtered_results = []
-    items = results_json['items']
+    # Extracts only relevant content from results json and returns a
+    # list of dict objects each containing the following content:
+    # - title
+    # - snippet
+    # - url (aka formatted url)
+    def get_filtered_results(self, results_json: dict) -> list:
+        filtered_results = []
+        items = results_json['items']
 
-    for item in items:
-        search_result = {
-            'title': item['title'] if 'title' in item else None,
-            'snippet': item['snippet'] if 'snippet' in item else None,
-            'url': item['formattedUrl'] if 'formattedUrl' in item else None
-        }
-        filtered_results.append(search_result)
+        for item in items:
+            search_result = {
+                'title': item['title'] if 'title' in item else None,
+                'snippet': item['snippet'] if 'snippet' in item else None,
+                'url': item['formattedUrl'] if 'formattedUrl' in item else None
+            }
+            filtered_results.append(search_result)
 
-    return filtered_results
+        return filtered_results
 
+    # Performs a search for the given query. Returns max 100 results
+    def search(self, query: str) -> list:
+        key, engineID = self.get_credentials()
+        filtered_results_all = []
 
-# Performs a search for the given query. Returns max 100 results
-def search(query: str) -> list:
-    key, engineID = get_credentials()
-    filtered_results_all = []
+        # Each API call returns max 10 results. Perform the call 10 times to retrieve (max) all 100 results
+        for i in range(10):
+            payload = {'key': key, 'cx': engineID, 'q': query, 'start': i*10}
+            res = requests.get(self.URL, params=payload)
+            res_json = res.json()
 
-    # Each API call returns max 10 results. Perform the call 10 times to retrieve (max) all 100 results
-    for i in range(10):
-        payload = {'key': key, 'cx': engineID, 'q': query, 'start': i*10}
-        res = requests.get(URL, params=payload)
-        res_json = res.json()
+            # Check for maximum API quota exceeded
+            if 'error' in res_json:
+                print(res_json)
+                return []
 
-        # Check for maximum API quota exceeded
-        if 'error' in res_json:
-            print(res_json)
-            return []
+            filtered_res = self.get_filtered_results(res_json)
 
-        filtered_res = get_filtered_results(res_json)
+            filtered_results_all += filtered_res
 
-        filtered_results_all += filtered_res
+            # Exit if last page or no more results
+            if len(filtered_res) < 10:
+                break
 
-        # Exit if last page or no more results
-        if len(filtered_res) < 10:
-            break
+        return filtered_results_all
 
-    return filtered_results_all
-
-
-# Sample search
-def test_search():
-    filtered_results = search('cat')
-    print('Number of results:', len(filtered_results))
-    with open('testing_data/filtered_results.json', 'w') as f:
-        f.write(json.dumps(filtered_results, indent=4))
+    # Sample search
+    def test_search(self):
+        filtered_results = self.search('cat')
+        print('Number of results:', len(filtered_results))
+        with open('testing_data/filtered_results.json', 'w') as f:
+            f.write(json.dumps(filtered_results, indent=4))
 
 
 if __name__ == '__main__':
-    test_search()
+    x = GoogleCustomSearch()
+    x.test_search()
--- src/apis/google_scholar.py
+++ src/apis/google_scholar.py
@@ -6,55 +6,57 @@ from dotenv import load_dotenv
 from serpapi.google_search_results import GoogleSearchResults
 
 
-# Google API credentials
-def get_credentials() -> str:
-    load_dotenv()
-    return os.getenv('SERPAPI_KEY')
+class GoogleScholar:
+    def __init__(self):
+        pass
 
+    # Google API credentials
+    def get_credentials(self) -> str:
+        load_dotenv()
+        return os.getenv('SERPAPI_KEY')
 
-# Extracts only relevant content from results json and returns a 
-# list of dict objects each containing the following content:
-# - title
-# - snippet
-# - url (aka formatted url)
-def get_filtered_results(results_json: dict) -> list:
-    filtered_results = []
+    # Extracts only relevant content from results json and returns a
+    # list of dict objects each containing the following content:
+    # - title
+    # - snippet
+    # - url (aka formatted url)
+    def get_filtered_results(self, results_json: dict) -> list:
+        filtered_results = []
 
-    for item in results_json:
-        search_result = {
-            'title': item['title'] if 'title' in item else None,
-            'snippet': item['snippet'] if 'snippet' in item else None,
-            'url': item['link'] if 'link' in item else None
+        for item in results_json:
+            search_result = {
+                'title': item['title'] if 'title' in item else None,
+                'snippet': item['snippet'] if 'snippet' in item else None,
+                'url': item['link'] if 'link' in item else None
+            }
+            filtered_results.append(search_result)
+
+        return filtered_results
+
+    # Performs a search for the given query
+    def search(self, query: str) -> list:
+        key = self.get_credentials()
+
+        params = {
+            'engine': 'google_scholar',
+            'q': query,
+            'api_key': key
         }
-        filtered_results.append(search_result)
 
-    return filtered_results
+        client = GoogleSearchResults(params)
+        res = client.get_dict()
 
+        filtered_res = self.get_filtered_results(res['organic_results'])
+        return filtered_res
 
-# Performs a search for the given query
-def search(query: str) -> list:
-    key = get_credentials()
-
-    params = {
-        'engine': 'google_scholar',
-        'q': query,
-        'api_key': key
-    }
-
-    client = GoogleSearchResults(params)
-    res = client.get_dict()
-
-    filtered_res = get_filtered_results(res['organic_results'])
-    return filtered_res
-
-
-# Sample search
-def test_search():
-    filtered_results = search('cat')
-    print('Number of results:', len(filtered_results))
-    with open('testing_data/filtered_results.json', 'w') as f:
-        f.write(json.dumps(filtered_results, indent=4))
+    # Sample search
+    def test_search(self):
+        filtered_results = self.search('cat')
+        print('Number of results:', len(filtered_results))
+        with open('testing_data/filtered_results.json', 'w') as f:
+            f.write(json.dumps(filtered_results, indent=4))
 
 
 if __name__ == '__main__':
-    test_search()
+    x = GoogleScholar()
+    x.test_search()
