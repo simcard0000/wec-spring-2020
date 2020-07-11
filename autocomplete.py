649a952c30435cdf278bc1924949b8e2c61b4300
a-gratton <gratton.aidan@gmail.com>
2020-07-11 08:24:36 -0600

    Completed API Setup

--- autocomplete.py
+++ autocomplete.py
@@ -1,8 +1,22 @@
 import requests
 
-response = requests.get(
-    "https://eastus.api.cognitive.microsoft.com/bing/v7.0/suggestions",
-    params={'q': 'hello', 'Ocp-Apim-Subscription-Key': 'f778d2410d5948398ea947128b325e48'}
-)
+key = ''
+url = "https://eastus.api.cognitive.microsoft.com/bing/v7.0/suggestions"
+
+with open('auth.txt', 'r') as file:
+    data = file.read().split(' ')
+    for i, x in enumerate(data):
+        if x == 'Ocp-Apim-Subscription-Key':
+            key = data[i+1]
+
+
+payload = {}
+params = {'q': "hello"}
+headers = {
+'Ocp-Apim-Subscription-Key': key
+}
+
+
+response = requests.get(url, headers=headers, params=params, data=payload)
 
 print(response.content)
